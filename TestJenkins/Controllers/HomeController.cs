0a277420330a8975ad67b4add497e204e543c4e6
xyy <1656523730@qq.com>
2022-02-24 19:52:57 +0800

    修改数据

--- TestJenkins/Controllers/HomeController.cs
+++ TestJenkins/Controllers/HomeController.cs
@@ -11,7 +11,7 @@ namespace TestJenkins.Controllers
 		[HttpGet]
 		public IActionResult Index()
 		{
-			string  str="HelloWord";
+			string  str="HelloWord  jenkins";
 			return   Content(str);    
 		}
 	}
